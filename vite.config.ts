0651b72660d2d9f8c084afebc140ccf62fb121b1
Animesh <anime@example.com>
2026-04-23 09:40:48 +0530

    Clean up comments in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 
-// Replace 'Personal-Portfolio' with the exact name of your GitHub repository if it's different!
 export default defineConfig({
   base: '/Personal-Portfolio/',
 })
